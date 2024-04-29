657f87c8a54dcf2cc004258a59a81c039d22452c
ferferzada <88342708+ferferzada@users.noreply.github.com>
2024-04-29 18:59:20 -0300

    create header

---
+++ shopping_cart/src/app/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+export function Header(){
+
+    return (
+        <header>
+            <h1>Portfólio</h1>
+            <nav>
+                <a href="#">
+                    About
+                </a>
+                <a href="#">
+                    Projects
+                </a>
+            </nav>
+        </header>
+
+    );
+
+}
+
+
+export default Header;
